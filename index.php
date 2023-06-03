19d97c9d1bc2657237cfbc35003cd5e2816c29c0
Md Golam Morshed <morshedseu.49@gmail.com>
2023-06-03 23:14:04 +0600

    two  commit

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculator</title>
+
     <!-- Bootstrap cdn here  -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
@@ -26,10 +26,7 @@
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary mt-4">Calculation</button>
                     </div>
-                    
                 </form>
-
-
             </div>
         </div>
     </div>
@@ -38,5 +35,4 @@
     <!-- js cdn here  -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js">
 </body>
-
 </html>
